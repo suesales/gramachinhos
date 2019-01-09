a68af35a327725424b1ab1e0bbc821238dc49854
Luis Paulo Jr <luispaulojr@gmail.com>
2019-01-09 15:54:58 -0200

    ajuste de conexao com o bd

--- connection.php
+++ connection.php
@@ -1,9 +1,9 @@
 <?php
 
-  define("__SERVER__", "localhost");
-  define("__USER__", "root");
-  define("__PASS__", "");
-  define("__DB__", "gramachinhos");
+  define("__SERVER__", "ol5tz0yvwp930510.cbetxkdyhwsb.us-east-1.rds.amazonaws.com");
+  define("__USER__", "n3oo0h0sfg6xlg01");
+  define("__PASS__", "eixkcfgx1637kptz");
+  define("__DB__", "ammixai60dixbzla");
   define("__PORT__", 3306);
 
   function getConnection() {
